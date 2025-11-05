a31cdb6b2eae59af4873ef7fa2c0f86d75ebf893
Nachi-FCX <nachiyappan@fincorpx.com>
2025-11-05 16:02:40 +0530

    fix v3

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+  },
 })
